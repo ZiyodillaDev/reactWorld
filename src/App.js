58135b59ae48fd664e3bf6498f40d4dbf11a13ad
ZiyodillaDev <ziyodillazokirkhujayev@gmail.com>
2023-01-20 17:03:57 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 
-
+import Header from "./components/header";
+import "./components/header.css"
+import Form from "./components/form";
+import "./components/form.css"
+import Card from "./components/card";
+import "./components/card.css"
 function App() {
   return (
     <>
-      
+     <Header />
+     <Form />
+     <Card/>
     </>
   )
 }
